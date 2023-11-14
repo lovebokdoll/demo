95646425eb65343c7793ad82912a879b53d63274
lovebokdoll <dltmfekgml@naver.com>
2023-11-14 12:05:38 +0900

    build Test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	// 여기를 실행시키면된다!!
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
